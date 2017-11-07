ea5fa92144af05c542ac5c14d17684df60190aec
neoy.yang@jieweifu.com <123456>
2017-11-07 15:57:01 +0800

    接OpenAPI

--- application/src/main/java/com/jieweifu/common/utils/HttpUtil.java
+++ application/src/main/java/com/jieweifu/common/utils/HttpUtil.java
@@ -1,56 +1,49 @@
 package com.jieweifu.common.utils; /**
- *
- **************************************************************************************
- *
- *  Project:        ZXQ
- *
- *  Copyright ©     2014-2017 Banma Technologies Co.,Ltd
- *                  All rights reserved.
- *
- *  This software is supplied only under the terms of a license agreement,
- *  nondisclosure agreement or other written agreement with Banma Technologies
- *  Co.,Ltd. Use, redistribution or other disclosure of any parts of this
- *  software is prohibited except in accordance with the terms of such written
- *  agreement with Banma Technologies Co.,Ltd. This software is confidential
- *  and proprietary information of Banma Technologies Co.,Ltd.
- *
- **************************************************************************************
- *
- *  Class Name: com.test.MyTest.java
- *
- *  General Description:
- *
- *  Revision History:
- *                           Modification
- *   Author                Date(MM/DD/YYYY)   JiraID           Description of Changes
- *   ---------------------   ------------    ----------     -----------------------------
- *   LZY                   2017年8月29日
- *
- ***************************************************************************************
- *
+ * *************************************************************************************
+ * <p>
+ * Project:        ZXQ
+ * <p>
+ * Copyright ©     2014-2017 Banma Technologies Co.,Ltd
+ * All rights reserved.
+ * <p>
+ * This software is supplied only under the terms of a license agreement,
+ * nondisclosure agreement or other written agreement with Banma Technologies
+ * Co.,Ltd. Use, redistribution or other disclosure of any parts of this
+ * software is prohibited except in accordance with the terms of such written
+ * agreement with Banma Technologies Co.,Ltd. This software is confidential
+ * and proprietary information of Banma Technologies Co.,Ltd.
+ * <p>
+ * *************************************************************************************
+ * <p>
+ * Class Name: com.test.MyTest.java
+ * <p>
+ * General Description:
+ * <p>
+ * Revision History:
+ * Modification
+ * Author                Date(MM/DD/YYYY)   JiraID           Description of Changes
+ * ---------------------   ------------    ----------     -----------------------------
+ * LZY                   2017年8月29日
+ * <p>
+ * **************************************************************************************
  */
 
 
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 
 import net.sf.json.JSONObject;
-import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 
@@ -64,37 +57,40 @@ public class HttpUtil {
 
     /**
      * 发送HttpGet请求
+     *
      * @param url
      * @return
      */
-    public static JSONObject sendGet(String url,String appId,String token,Map<String,String> map) {
+    public static JSONObject sendGet(String url, String appId, String token, Map<String, String> map) {
 
         StringBuilder params = new StringBuilder();
-        map.forEach(
-                (s, s2) ->
-                    params.append(s+":"+s2+"&&")
+        if (map != null)
+            map.forEach(
+                    (s, s2) ->
+                            params.append(s + ":" + s2 + "&&")
 
-        );
+            );
         HttpGet httpget = null;
-        httpget = new HttpGet(url+"?"+ params);
-        httpget.setHeader("X-Gizwits-Application-Id",appId);
-        if(token!=null){
-            httpget.setHeader("X-Gizwits-User-token",token);
+        httpget = new HttpGet(url + "?" + params);
+        httpget.setHeader("X-Gizwits-Application-Id", appId);
+        if (token != null) {
+            httpget.setHeader("X-Gizwits-User-token", token);
         }
-        System.out.println(url+"?"+ params);
         CloseableHttpResponse response = null;
         try {
             response = httpclient.execute(httpget);
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-        JSONObject result = null;
+        String result = "";
+        JSONObject jsonObject = null;
         try {
             HttpEntity entity1 = response.getEntity();
             if (entity1 != null) {
-                result = JSONObject.fromObject(entity1);
+                result = EntityUtils.toString(entity1);
+                jsonObject = JSONObject.fromObject(result.replaceAll("null", "''"));
             }
-        } catch (ParseException e) {
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         } finally {
             try {
@@ -103,31 +99,35 @@ public class HttpUtil {
                 e.printStackTrace();
             }
         }
-        return result;
+        return jsonObject;
     }
 
     /**
      * 发送参数的HttpPost请求
+     *
      * @param url
      * @return
      */
-    public static JSONObject sendPost(String url,String appId,String token,String auth,Map<String, String> map) {
+    public static JSONObject sendPost(String url, String appId, String token, String auth, JSONObject json) {
 
         HttpPost httppost = new HttpPost(url);
-        httppost.setHeader("X-Gizwits-Application-Id",appId);
-        if(token!=null){
-            httppost.setHeader("X-Gizwits-User-token",token);
+        httppost.setHeader("X-Gizwits-Application-Id", appId);
+        if (token != null) {
+            httppost.setHeader("X-Gizwits-User-token", token);
         }
-        if(auth!=null){
-            httppost.setHeader("X-Gizwits-Application-Auth",auth);
+        if (auth != null) {
+            httppost.setHeader("X-Gizwits-Application-Auth", auth);
         }
-        if(map!=null){
-            List<NameValuePair> formparams = new ArrayList<>();
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                formparams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+        if (json != null) {
+            StringEntity s = null;
+            try {
+                s = new StringEntity(json.toString());
+                s.setContentEncoding("UTF-8");
+                s.setContentType("application/json");
+                httppost.setEntity(s);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
-            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
-            httppost.setEntity(entity);
         }
         CloseableHttpResponse response = null;
         try {
@@ -136,36 +136,45 @@ public class HttpUtil {
             e.printStackTrace();
         }
         HttpEntity entity1 = response.getEntity();
-        JSONObject result = null;
+        JSONObject jsonObject = null;
+        String result = "";
         try {
-            result = JSONObject.fromObject(entity1);
-        } catch (ParseException e) {
+            result = EntityUtils.toString(entity1);
+            jsonObject = JSONObject.fromObject(result.replaceAll("null", "''"));
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
-        return result;
+        return jsonObject;
     }
 
     /**
      * 发送HttpPut请求，参数为map
+     *
      * @param url
-     * @param map
+     * @param json
      * @return
      */
-    public static JSONObject sendPut(String url,String appId,String token,String auth, Map<String, String> map) {
-        List<NameValuePair> formparams = new ArrayList<>();
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            formparams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-        }
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+    public static JSONObject sendPut(String url, String appId, String token, String auth, JSONObject json) {
+
         HttpPut httpPut = new HttpPut(url);
-        httpPut.setHeader("X-Gizwits-Application-Id",appId);
-        if(token!=null){
-            httpPut.setHeader("X-Gizwits-User-token",token);
+        httpPut.setHeader("X-Gizwits-Application-Id", appId);
+        if (token != null) {
+            httpPut.setHeader("X-Gizwits-User-token", token);
         }
-        if(auth!=null){
-            httpPut.setHeader("X-Gizwits-Application-Auth",auth);
+        if (auth != null) {
+            httpPut.setHeader("X-Gizwits-Application-Auth", auth);
         }
-        httpPut.setEntity(entity);
+        if (json != null) {
+            StringEntity s = null;
+            try {
+                s = new StringEntity(json.toString());
+                s.setContentEncoding("UTF-8");
+                s.setContentType("application/json");
+                httpPut.setEntity(s);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
         CloseableHttpResponse response = null;
         try {
             response = httpclient.execute(httpPut);
@@ -173,17 +182,16 @@ public class HttpUtil {
             e.printStackTrace();
         }
         HttpEntity entity1 = response.getEntity();
-        JSONObject result = null;
+        JSONObject jsonObject = null;
+        String result = "";
         try {
-            result = JSONObject.fromObject(entity1);
-        } catch (ParseException e) {
+            result = EntityUtils.toString(entity1);
+            jsonObject = JSONObject.fromObject(result.replaceAll("null", "''"));
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
-        return result;
+        return jsonObject;
     }
-
-
-
 
 }
   
--- application/src/main/java/com/jieweifu/controllers/GizWits/UsersController.java
+++ application/src/main/java/com/jieweifu/controllers/GizWits/UsersController.java
@@ -1,7 +1,6 @@
 package com.jieweifu.controllers.GizWits;
 
 
-import com.jieweifu.common.business.BaseContextHandler;
 import com.jieweifu.common.utils.HttpUtil;
 import com.jieweifu.common.utils.Md5Util;
 import com.jieweifu.models.Result;
@@ -19,142 +18,226 @@ import java.util.Map;
 @RequestMapping("giz/users")
 public class UsersController {
 
-    @GetMapping("getAppUsers/{id}")
-    public Result getAppUsers(@PathVariable("id") String id) {
+    public static String Token = null;
+
+
+    /**
+     * 得到用户信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("getAppUsers/{id}/{appsecret}")
+    public Result getAppUsers(@PathVariable("id") String id, @PathVariable("appsecret") String appsecret) {
         if (id.matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数错误");
         }
         Map<String, String> map = new HashMap<>();
-        String token = getToken();
-        JSONObject jsonObject = HttpUtil.sendGet("http://api.gizwits.com/app/users", id, token, null);
+        JSONObject jsonObject = HttpUtil.sendGet("http://api.gizwits.com/app/users", id, getToken(id, appsecret), null);
         return new Result().setData(jsonObject);
     }
 
+
+    /**
+     * 创建用户
+     *
+     * @param map
+     * @param id
+     * @return
+     */
     @PostMapping("postAppUsers/{id}")
     public Result postAppUsers(@RequestBody Map<String, String> map, @PathVariable("id") String id) {
         if (id.matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数不能为空");
         }
-        JSONObject result = HttpUtil.sendPost("http://api.gizwits.com/app/users", id, null, null, map);
-        return new Result().setData(result);
+        JSONObject json = JSONObject.fromObject(map);
+        JSONObject jsonObject = HttpUtil.sendPost("http://api.gizwits.com/app/users", id, null, null, json);
+        return new Result().setData(jsonObject);
     }
 
-    @PutMapping("putAppUsers/{id}")
-    public Result putAppUsers(@RequestBody Map<String, String> map, @PathVariable("id") String id) {
+    /**
+     * 修改用户信息
+     *
+     * @param map
+     * @param id
+     * @return
+     */
+    @PutMapping("putAppUsers/{id}/{appsecret}")
+    public Result putAppUsers(@RequestBody Map<String, String> map,
+                              @PathVariable("id") String id,
+                              @PathVariable("appsecret") String appsecret) {
         if (id.matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数错误");
         }
-        String token = getToken();
-        JSONObject result = HttpUtil.sendPut("http://api.gizwits.com/app/users", id, token, null, map);
+        JSONObject json = JSONObject.fromObject(map);
+        JSONObject result = HttpUtil.sendPut("http://api.gizwits.com/app/users", id, getToken(id, appsecret), null, json);
         return new Result().setData(result);
     }
 
-
-    @PostMapping("postAppLogin/{id}")
-    public Result postAppLogin(@RequestBody Map<String, String> map, @PathVariable("id") String id) {
+    /**
+     * 用户登录
+     *
+     * @param map
+     * @param id
+     * @return
+     */
+    @PostMapping("postAppLogin/{id}/{appsecret}")
+    public Result postAppLogin(@RequestBody Map<String, String> map,
+                               @PathVariable("id") String id,
+                               @PathVariable("appsecret") String appsecret) {
         if (id.matches(Regex.NOTNULL_REX)
                 || map.get("username").matches(Regex.NOTNULL_REX) || map.get("password").matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数错误");
         }
-        String token = getToken();
-        JSONObject result = HttpUtil.sendPost("http://api.gizwits.com/app/login", id, token, null, map);
-        return new Result().setData(result);
-    }
-
-    public String getToken() {
-        String id = (String) BaseContextHandler.get("appId");
-        String appsecret = (String) BaseContextHandler.get("appsecret");
-        return postAppRequestToken(id, appsecret).getData().toString();
+        JSONObject json = JSONObject.fromObject(map);
+        JSONObject jsonObject = HttpUtil.sendPost("http://api.gizwits.com/app/login", id, getToken(id, appsecret), null, json);
+        return new Result().setData(jsonObject);
     }
 
     /**
-     * 获取Token
+     * Token调用方法
      *
      * @param id
      * @param appsecret
      * @return
      */
+    public String getToken(String id, String appsecret) {
+        if ((id.matches(Regex.NOTNULL_REX)) || (appsecret.matches(Regex.NOTNULL_REX))) {
+            return null;
+        }
+        String auth = Md5Util.encrypt32(id + appsecret);
+        JSONObject jsonObject = HttpUtil.sendPost("http://api.gizwits.com/app/request_token", id, null, auth, null);
+        String token = jsonObject.get("token").toString();
+        return token;
+    }
+
+    /**
+     * 获取Token
+     *
+     * @param map
+     * @return
+     */
     @PostMapping("postAppRequestToken")
-    public Result postAppRequestToken(@RequestBody String id, @RequestBody String appsecret) {
+    public Result postAppRequestToken(@RequestBody Map<String, String> map) {
+
+        String id = map.get("id");
+        String appsecret = map.get("appsecret");
         if ((id.matches(Regex.NOTNULL_REX)) || (appsecret.matches(Regex.NOTNULL_REX))) {
             return new Result().setError("参数不合法");
         }
         String auth = Md5Util.encrypt32(id + appsecret);
-        Map<String, String> map = new HashMap<>();
-        JSONObject result = HttpUtil.sendPost("http://api.gizwits.com/app/request_token", id, null, auth, null);
-        return new Result().setData(result);
+        JSONObject jsonObject = HttpUtil.sendPost("http://api.gizwits.com/app/request_token", id, null, auth, null);
+        Token = jsonObject.get("token").toString();
+        return new Result().setData(jsonObject);
     }
 
+    /**
+     * 重置密码
+     *
+     * @param map
+     * @param id
+     * @return
+     */
     @PostMapping("postAppResetPassword/{id}")
     public Result postAppResetPassword(@RequestBody Map<String, String> map, @PathVariable("id") String id) {
-        if (id.matches(Regex.NOTNULL_REX)){
+        if (id.matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数错误");
         }
-        if (!map.get("email").matches(Regex.EMAIL_REX)
-                || !map.get("phone").matches(Regex.PHONE_REX)) {
-            if (!map.get("new_pwd").matches(Regex.PASSWORD_REX) &&
-                    !map.get("code").matches(Regex.NOTNULL_REX)) {
-                HttpUtil.sendPost("http://api.gizwits.com/app/reset_password", id, null, null, map);
-                return new Result().setMessage("重置密码短信/邮件发送成功,请及时查收");
+        if (map.get("email").matches(Regex.EMAIL_REX)
+                || map.get("phone").matches(Regex.PHONE_REX)) {
+            if (map.get("new_pwd").matches(Regex.PASSWORD_REX) &&
+                    map.get("code").matches(Regex.NOTNULL_REX)) {
+                JSONObject json = JSONObject.fromObject(map);
+                JSONObject jsonObject = HttpUtil.sendPost("http://api.gizwits.com/app/reset_password", id, null, null, json);
+                return new Result().setData(jsonObject);
             }
         }
         return new Result().setError("请完善重置密码必要的信息");
     }
 
     /**
-     * @param map X-Gizwits-Application-Id   appsecret   phone  code
+     * 验证码的获取和校验
+     *
+     * @param map
      * @return
      */
-    @PostMapping("postAppSmsCode/{id}")
-    public Result postAppSmsCode(@RequestBody Map<String, String> map, @PathVariable("id") String id) {
+    @PostMapping("postAppSmsCode/{id}/{appsecret}")
+    public Result postAppSmsCode(@RequestBody Map<String, String> map,
+                                 @PathVariable("id") String id,
+                                 @PathVariable("appsecret") String appsecret) {
         if (id.matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数错误");
         }
         if (map.get("phone").matches(Regex.PHONE_REX) && !map.get("code").matches(Regex.NOTNULL_REX)) {
-            String token = getToken();
-            HttpUtil.sendPost("http://api.gizwits.com/app/sms_code", id, token, null, map);
-            return new Result().setMessage("发送短信成功");
+            JSONObject json = JSONObject.fromObject(map);
+            JSONObject jsonObject = HttpUtil.sendPost("http://api.gizwits.com/app/sms_code", id, getToken(id, appsecret), null, json);
+            return new Result().setData(jsonObject);
         }
         return new Result().setMessage("手机号或验证码有误");
     }
 
-    @GetMapping("getAppVerifyCodes/{id}")
-    public Result getAppVerifyCodes(@PathVariable String id) {
+    /**
+     * 获取图片验证码
+     *
+     * @param id
+     * @param appsecret
+     * @return
+     */
+    @GetMapping("getAppVerifyCodes/{id}/{appsecret}")
+    public Result getAppVerifyCodes(@PathVariable String id,
+                                    @PathVariable("appsecret") String appsecret) {
         if (id.matches(Regex.NOTNULL_REX)) {
             return new Result().setError("参数错误");
         }
-        String token = getToken();
-        JSONObject result = HttpUtil.sendGet("http://api.gizwits.com/app/verify/codes", id, token, null);
-        return new Result().setData(result);
+        JSONObject jsonObject = HttpUtil.sendGet("http://api.gizwits.com/app/verify/codes", id, getToken(id, appsecret), null);
+        return new Result().setData(jsonObject);
     }
 
-
-    @PostMapping("postAppVerifyCodes/{id}")
-    public Result postAppVerifyCodes(@RequestBody Map<String, String> map, @PathVariable("id") String id) {
+    /**
+     * 校验图片验证码并发送验证码
+     *
+     * @param map
+     * @param id
+     * @param appsecret
+     * @return
+     */
+    @PostMapping("postAppVerifyCodes/{id}/{appsecret}")
+    public Result postAppVerifyCodes(@RequestBody Map<String, String> map,
+                                     @PathVariable("id") String id,
+                                     @PathVariable("appsecret") String appsecret) {
         if (id.matches(Regex.NOTNULL_REX) ||
                 map.get("captcha_id").matches(Regex.NOTNULL_REX) ||
                 map.get("captcha_code").matches(Regex.NOTNULL_REX) ||
                 map.get("phone").matches(Regex.NOTNULL_REX)) {
             return new Result().setError("请完善信息");
         }
-        String token = getToken();
-        HttpUtil.sendPost("http://api.gizwits.com/app/verify/codes", id, token, null, map);
+        JSONObject json = JSONObject.fromObject(map);
+        HttpUtil.sendPost("http://api.gizwits.com/app/verify/codes", id, getToken(id, appsecret), null, json);
         return new Result().setMessage("短信已发送");
 
     }
 
-    @GetMapping("putAppVerifyCodes/{id}/{phone}/{code}")
+    /**
+     * 校验短信验证码
+     *
+     * @param id
+     * @param phone
+     * @param code
+     * @param appsecret
+     * @return
+     */
+    @GetMapping("putAppVerifyCodes/{id}/{phone}/{code}/{appsecret}")
     public Result putAppVerifyCodes(@PathVariable("id") String id,
                                     @PathVariable("phone") String phone,
-                                    @PathVariable("code") String code) {
+                                    @PathVariable("code") String code,
+                                    @PathVariable("appsecret") String appsecret) {
         if (!id.matches(Regex.NOTNULL_REX) &&
                 !phone.matches(Regex.NOTNULL_REX) && !code.matches(Regex.NOTNULL_REX)) {
             Map<String, String> map = new HashMap<>();
             map.put("phone", phone);
             map.put("sms_code", code);
-            String token = getToken();
-            HttpUtil.sendGet("https://api.gizwits.com/app/verify/codes",id,token,map);
-            return new Result().setMessage("校验成功");
+            JSONObject jsonObject = HttpUtil.sendGet("https://api.gizwits.com/app/verify/codes", id, getToken(id, appsecret), map);
+            return new Result().setData(jsonObject);
         }
         return new Result().setError("校验失败");
     }
