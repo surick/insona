38c2abe0a5e8ccc8876055a4e00a3d7c5fed1bf2
neoy2 <neoy@jieweifu.com>
2018-01-12 19:07:00 +0800

    菜单优化，角色配置权限优化，用户信息优化，设备批次改进，设备类别与设备新增优化

--- view/src/views/insona/sale-expand.vue
+++ view/src/views/insona/sale-expand.vue
@@ -6,12 +6,10 @@
             <span class="expand-value">{{ row.extract }}</span>
             </Col>
         </Row>
-    </div>
-    <div>
         <Row class="expand-row">
             <Col>
-            <span class="expand-key">详细信息：</span>
-            <span class="expand-value">{{ row.reason }}</span>
+            <span class="expand-key">驳回原因：</span>
+            <span class="expand-value">{{ row.reason=='' ?'审批中': row.reason }}</span>
             </Col>
         </Row>
     </div>
